e97e3e5b78176a9ac57901c8c7c363f03cf51852
Maciel <macielgaleano.jh@gmail.com>
2020-09-11 15:17:42 -0300

    Need fix nav

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@ $(document).ready(function () {
     $(".fa-arrow-right").css("transform", "rotate:0deg");
   });
 
+  $(window).resize(function () {
+    if ($(window).width() < 963) {
+      menu.addClass("bg-dark");
+      menu.addClass("nav-margin");
+      console.log(menu);
+    } else {
+      menu.removeClass("nav-margin");
+    }
+  });
+
   $(window).scroll(function () {
     console.log("funca");
     var menu = $("#menu");
@@ -14,13 +24,6 @@ $(document).ready(function () {
       menu.addClass("bg-inverse");
     } else {
       menu.removeClass("bg-inverse");
-    }
-
-    if ($(window).width() < 963) {
-      menu.addClass("bg-dark");
-      menu.addClass("nav-margin");
-    } else {
-      menu.removeClass("nav-margin");
     }
   });
 });
